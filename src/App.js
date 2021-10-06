29c6cb8966ceece96d8f3d9f239b027b7d595d58
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-10-06 11:17:01 -0300

    Estrutura Inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      
     </div>
   );
 }
