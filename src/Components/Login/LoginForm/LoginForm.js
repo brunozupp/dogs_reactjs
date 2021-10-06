1311e277a3034fcbdd0283b9e159e647bdc7650d
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-10-06 16:28:41 -0300

    Login Form

--- src/Components/Login/LoginForm/LoginForm.js
+++ src/Components/Login/LoginForm/LoginForm.js
@@ -5,6 +5,9 @@ import { UserContext } from '../../../Contexts/UserContext';
 import useForm from '../../../Hooks/useForm';
 import Button from '../../Forms/Button/Button';
 import Input from '../../Forms/Input/Input';
+import Error from '../../Helper/Error/Error';
+import styles from './LoginForm.module.css';
+import stylesButton from '../../Forms/Button/Button.module.css';
 
 const LoginForm = () => {
 
@@ -22,19 +25,22 @@ const LoginForm = () => {
     }
 
     return (
-        <section>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input label="Usuário" type="text" id="username" {...username} />
                 <Input label="Senha" type="password" id="password" {...password} />
                 {
                     loading ? <Button disabled>Carregando...</Button> : <Button>Entrar</Button>
                 }
-                {
-                    error && <p>{error}</p>
-                }
+                <Error error={error} />
             </form>
-            <Link to="/login/create">Cadastro</Link>
+            <Link className={styles.forgetPassword} to="/login/forget-password">Esqueceu a senha?</Link>
+            <div className={styles.register}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link className={stylesButton.button} to="/login/create">Cadastro</Link>
+            </div>
         </section>
     )
 }
