b872565dd1990e4cd37b9d9016d2caf120484ca7
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-10-08 00:51:35 -0300

    Biblioteca externa - graficos victory

--- src/Components/User/UserStatistics/UserStatistics.js
+++ src/Components/User/UserStatistics/UserStatistics.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import { STATS_GET } from '../../../API/api'
+import useFetch from '../../../Hooks/useFetch'
 import Head from '../../Head/Head'
+import Error from '../../Helper/Error/Error'
+import Loading from '../../Loading/Loading'
+import styles from './UserStatistics.module.css'
+import UserStatsGraphs from './UserStatsGraphs/UserStatsGraphs'
 
 const UserStatistics = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-            Estatísticas
-        </div>
-    )
+
+    const {data, error, loading, request} = useFetch();
+
+    React.useEffect(() => {
+
+        async function getData() {
+            const {url, options} = STATS_GET();
+
+            await request(url, options);
+        }
+
+        getData();
+    }, []);
+
+    if(loading) return <Loading />
+
+    if(error) return <Error error={error} />
+
+    if(data)
+        return (
+            <div>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </div>
+        );
+    
+    return null;
 }
 
 export default UserStatistics
